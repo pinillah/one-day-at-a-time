03572a3a72daffee82410e17c31aaf82e518c482
Juliana <pini0010@algonquinlive.com>
2023-11-08 00:07:06 -0500

    Completes the assignment.

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
-// Form Selection - Making it coherent with actual dates
+// Form Selection - Making it coherent with actual dates and database
 
-// Declaring variables to be used
-const monthSelect = document.getElementById('month');
-const daySelect = document.getElementById('day');
-const yearSelect = document.getElementById('year');
+// Declaring variables to be used from the form
+const month = document.getElementById('month');
+const day = document.getElementById('day');
+const year = document.getElementById('year');
 
 
-// Year options from the current year until 1995 
+// Year options from the current year until 1995 (as per NASA database)
 const currentYear = new Date().getFullYear();
 for (let i = currentYear; i >= currentYear - 28; i--) {
     const option = document.createElement('option');
     option.value = i;
     option.text = i;
-    yearSelect.appendChild(option);
+    year.appendChild(option);
 }
 
 // Function to show days based on the selected month and year
 function updateDays() {
-    const selectedMonth = parseInt(monthSelect.value);
-    const selectedYear = parseInt(yearSelect.value);
+    const selectedMonth = parseInt(month.value);
+    const selectedYear = parseInt(year.value);
 
     // Clear existing day options
-    daySelect.innerHTML = '<option value="0">Day</option>';
+    day.innerHTML = '<option value="0">day</option>';
 
     if (selectedMonth !== 0 && selectedYear !== 0) {
         const daysInMonth = new Date(selectedYear, selectedMonth, 0).getDate();
@@ -29,14 +29,14 @@ function updateDays() {
             const option = document.createElement('option');
             option.value = i;
             option.text = i;
-            daySelect.appendChild(option);
+            day.appendChild(option);
         }
     }
 }
 
-// Listen for changes in month and year selections
-monthSelect.addEventListener('change', updateDays);
-yearSelect.addEventListener('change', updateDays);
+// EventListener for changes in month and year selections
+month.addEventListener('change', updateDays);
+year.addEventListener('change', updateDays);
 
 // Initially populate days based on the current month and year
 updateDays();
@@ -45,21 +45,24 @@ updateDays();
 function restrictFutureDates() {
     const today = new Date();
     const selectedDate = new Date(
-        yearSelect.value,
-        monthSelect.value,
-        daySelect.value
+        year.value,
+        month.value,
+        day.value
     );
 
     if (selectedDate > today) {
         alert('Please select a date in the past or today.');
         // Reset the selects to prevent selecting a future date
-        monthSelect.value = '0';
-        daySelect.value = '0';
-        yearSelect.value = '0';
+        month.value = '0';
+        day.value = '0';
+        year.value = '0';
     }
 }
 
 // Listen for changes in all selects to restrict future dates
-monthSelect.addEventListener('change', restrictFutureDates);
-daySelect.addEventListener('change', restrictFutureDates);
-yearSelect.addEventListener('change', restrictFutureDates);
+month.addEventListener('change', restrictFutureDates);
+day.addEventListener('change', restrictFutureDates);
+year.addEventListener('change', restrictFutureDates);
+
+
+
